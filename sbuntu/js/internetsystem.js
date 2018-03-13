4aad411f7b4601b0dbc0ab38221ab5e2feb84032
hiroki-repo <hiroki7v11@gmail.com>
2018-03-13 16:46:59 +0900

    Update internetsystem.js

--- sbuntu/js/internetsystem.js
+++ sbuntu/js/internetsystem.js
@@ -144,7 +144,7 @@ function FirefoxSystem($parent){
 
 	function changeURL($url){
 	    	$('#submitURL ').attr('value', $url);
-		$url = 'https://nrird.xyz/proxy/index.php/' + $url
+		$url = 'https://hiroki7v11.miraiserver.net/pxy/proxy.php/' + $url
 	    	$('#firefoxInternet').attr('src', $url);
 	    	updateButtons();
 	    }
