e35170e718409bd2f32e68f4394dbd13ac742f32
hiroki-repo <hiroki7v11@gmail.com>
2018-06-04 18:33:00 +0900

    Update jslinux.js

--- freshlinux/jslinux.js
+++ freshlinux/jslinux.js
@@ -12,7 +12,7 @@ var term, pc, boot_start_time, init_state;
 
 function term_start()
 {
-    term = new Term(68, 34, term_handler);
+    term = new Term(68, 36, term_handler);
 
     term.open();
 }
