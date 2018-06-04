297c4034c78fac48a8131550587aec116020710f
hiroki-repo <hiroki7v11@gmail.com>
2018-06-04 18:48:14 +0900

    Update jslinux.js

--- freshlinux/jslinux.js
+++ freshlinux/jslinux.js
@@ -17,17 +17,6 @@ function term_start()
     term.open();
 }
 
-function urloptget()
-{
-var arg = new Object;
-var pair=location.search.substring(1).split('&');
-for(var i=0;pair[i];i++) {
-    var kv = pair[i].split('=');
-    arg[kv[0]]=kv[1];
-}
-return arg
-}
-
 /* send chars to the serial port */
 function term_handler(str)
 {
